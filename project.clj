a36921b8c341c9d1085b7f0e55d966fc18e20a10
Seth Schroeder <foogoof+gh@gmail.com>
2016-10-01 13:44:01 -0400

    might as well establish some perf benchmarks

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "DON'T BE A DICK PUBLIC LICENSE"
             :url "http://www.dbad-license.org"}
   :plugins [[lein-gorilla "0.3.6"]]
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/math.numeric-tower "0.0.4"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [criterium "0.4.4"]])
